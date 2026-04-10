d9d3152ab709036af9794c724dd1bae1d639d635
Thomas <surveycashweb@gmail.com>
2026-04-10 14:46:29 +0200

    Add my payments modal to cashout page

--- index.js
+++ index.js
@@ -4479,8 +4479,8 @@ cursor:pointer;
   display:flex;
   align-items:center;
   justify-content:center;
-  margin-top:10px;
-  margin-bottom:10px;
+  margin-top:6px;
+  margin-bottom:12px;
 }
 
           .method-logo-tile img{
